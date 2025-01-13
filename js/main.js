6d2d5f9e2bcadf3ef66eae224f6a5c47e159d68d
PhucHM <phuc.hm@outlook.com>
2025-01-13 20:39:31 +0700

    Remove unused Sass mixins and styles for cleaner codebase

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
-	var mobileMenuOutsideClick = function() {
+	function calculateDays() {
+		const startDateElement = document.getElementById('daysCount');
+		const startDate = new Date(startDateElement.getAttribute('data-start-date'));
+		const endDate = new Date(startDateElement.getAttribute('data-end-date'));
+		const currentDate = new Date();
+		const effectiveDate = currentDate > endDate ? endDate : currentDate;
+		const timeDifference = effectiveDate - startDate;
+		const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+		startDateElement.innerText = daysDifference;
+	}
+
+	var mobileMenuOutsideClick = function () {
 
 		$(document).click(function (e) {
-	    var container = $("#wd-offcanvas, .js-wd-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+			var container = $("#wd-offcanvas, .js-wd-nav-toggle");
+			if (!container.is(e.target) && container.has(e.target).length === 0) {
 
-	    	if ( $('body').hasClass('offcanvas') ) {
+				if ($('body').hasClass('offcanvas')) {
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-wd-nav-toggle').removeClass('active');
-	    	}
-	    }
+					$('body').removeClass('offcanvas');
+					$('.js-wd-nav-toggle').removeClass('active');
+				}
+			}
 		});
 
 	};
 
 
-	var offcanvasMenu = function() {
+	var offcanvasMenu = function () {
 
 		$('#page').prepend('<div id="wd-offcanvas" />');
 		$('#page').prepend('<a href="#" class="js-wd-nav-toggle wd-nav-toggle wd-nav-white"><i></i></a>');
@@ -34,42 +45,42 @@
 			.removeClass('has-dropdown');
 
 		// Hover dropdown menu on mobile
-		$('.offcanvas-has-dropdown').mouseenter(function(){
+		$('.offcanvas-has-dropdown').mouseenter(function () {
 			var $this = $(this);
 
 			$this
 				.addClass('active')
 				.find('ul')
-				.slideDown(500, 'easeOutExpo');				
-		}).mouseleave(function(){
+				.slideDown(500, 'easeOutExpo');
+		}).mouseleave(function () {
 
 			var $this = $(this);
 			$this
 				.removeClass('active')
 				.find('ul')
-				.slideUp(500, 'easeOutExpo');				
+				.slideUp(500, 'easeOutExpo');
 		});
 
 
-		$(window).resize(function(){
+		$(window).resize(function () {
 
-			if ( $('body').hasClass('offcanvas') ) {
+			if ($('body').hasClass('offcanvas')) {
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-wd-nav-toggle').removeClass('active');
-				
-	    	}
+				$('body').removeClass('offcanvas');
+				$('.js-wd-nav-toggle').removeClass('active');
+
+			}
 		});
 	};
 
 
-	var burgerMenu = function() {
+	var burgerMenu = function () {
 
-		$('body').on('click', '.js-wd-nav-toggle', function(event){
+		$('body').on('click', '.js-wd-nav-toggle', function (event) {
 			var $this = $(this);
 
 
-			if ( $('body').hasClass('overflow offcanvas') ) {
+			if ($('body').hasClass('overflow offcanvas')) {
 				$('body').removeClass('overflow offcanvas');
 			} else {
 				$('body').addClass('overflow offcanvas');
@@ -82,46 +93,46 @@
 
 
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight animated-fast');
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
+						}, k * 200, 'easeInOutExpo');
 					});
-					
+
 				}, 100);
-				
+
 			}
 
-		} , { offset: '85%' } );
+		}, { offset: '85%' });
 	};
 
 
-	var dropdown = function() {
+	var dropdown = function () {
 
-		$('.has-dropdown').mouseenter(function(){
+		$('.has-dropdown').mouseenter(function () {
 
 			var $this = $(this);
 			$this
@@ -129,7 +140,7 @@
 				.css('display', 'block')
 				.addClass('animated-fast fadeInUpMenu');
 
-		}).mouseleave(function(){
+		}).mouseleave(function () {
 			var $this = $(this);
 
 			$this
@@ -141,7 +152,7 @@
 	};
 
 
-	var testimonialCarousel = function(){
+	var testimonialCarousel = function () {
 		var owl = $('.owl-carousel-fullwidth');
 		owl.owlCarousel({
 			items: 1,
@@ -156,20 +167,20 @@
 	};
 
 
-	var goToTop = function() {
+	var goToTop = function () {
 
-		$('.js-gotop').on('click', function(event){
-			
+		$('.js-gotop').on('click', function (event) {
+
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
-			
+
 			return false;
 		});
 
-		$(window).scroll(function(){
+		$(window).scroll(function () {
 
 			var $win = $(window);
 			if ($win.scrollTop() > 200) {
@@ -179,120 +190,121 @@
 			}
 
 		});
-	
+
 	};
 
 
 	// Loading page
-	var loaderPage = function() {
+	var loaderPage = function () {
 		$(".wd-loader").fadeOut("slow");
 	};
 
-	var counter = function() {
+	var counter = function () {
 		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
+			formatter: function (value, options) {
+				return value.toFixed(options.decimals);
+			},
 		});
 	};
 
-	var counterWayPoint = function() {
-		if ($('#wd-counter').length > 0 ) {
-			$('#wd-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
+	var counterWayPoint = function () {
+		if ($('#wd-counter').length > 0) {
+			$('#wd-counter').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout(counter, 400);
 					$(this.element).addClass('animated');
 				}
-			} , { offset: '90%' } );
+			}, { offset: '90%' });
 		}
 	};
 
 	// Parallax
-	var parallax = function() {
+	var parallax = function () {
 		$(window).stellar();
 	};
 
-	
-	$(function(){
+
+	$(function () {
 		mobileMenuOutsideClick();
 		parallax();
 		// offcanvasMenu();
-		burgerMenu();
+		// burgerMenu();
 		contentWayPoint();
-		dropdown();
+		// dropdown();
 		testimonialCarousel();
 		goToTop();
 		loaderPage();
 		counter();
 		counterWayPoint();
 		// Force scroll to top after reload
-		$(window).on('beforeunload', function() {
+		$(window).on('beforeunload', function () {
 			$(window).scrollTop(0);
 		});
+		calculateDays();
 	});
 
-	document.addEventListener('DOMContentLoaded', function() {
-	    const playPauseBtn = document.getElementById('playPauseBtn');
-	    const backgroundMusic = document.getElementById('backgroundMusic');
-	    const notification = document.getElementById('notification');
-	    const overlayCover = document.getElementById('overlay-cover');
-	    const overlayTop = document.querySelector('.overlay-top');
-	    const overlayBottom = document.querySelector('.overlay-bottom');
-	    const overlayContent = document.querySelector('.overlay-content');
-	    let isPlaying = false;
+	document.addEventListener('DOMContentLoaded', function () {
+		const playPauseBtn = document.getElementById('playPauseBtn');
+		const backgroundMusic = document.getElementById('backgroundMusic');
+		const notification = document.getElementById('notification');
+		const overlayCover = document.getElementById('overlay-cover');
+		const overlayTop = document.querySelector('.overlay-top');
+		const overlayBottom = document.querySelector('.overlay-bottom');
+		const overlayContent = document.querySelector('.overlay-content');
+		let isPlaying = false;
 
-	    const handlePlayPause = () => {
-	        if (isPlaying) {
-	            backgroundMusic.pause();
-	            playPauseBtn.innerHTML = '<i class="icon-play"></i>';
-	            playPauseBtn.classList.remove('playing');
-	        } else {
-	            backgroundMusic.play();
-	            playPauseBtn.innerHTML = '<i class="icon-pause"></i>';
-	            playPauseBtn.classList.add('playing');
-	        }
-	        isPlaying = !isPlaying;
-	    };
+		const handlePlayPause = () => {
+			if (isPlaying) {
+				backgroundMusic.pause();
+				playPauseBtn.innerHTML = '<i class="icon-play"></i>';
+				playPauseBtn.classList.remove('playing');
+			} else {
+				backgroundMusic.play();
+				playPauseBtn.innerHTML = '<i class="icon-pause"></i>';
+				playPauseBtn.classList.add('playing');
+			}
+			isPlaying = !isPlaying;
+		};
 
-	    const handleOverlayClick = () => {
-	        overlayTop.classList.add('slide-up');
-	        overlayBottom.classList.add('slide-down');
-	        setTimeout(() => {
-	            overlayCover.style.display = 'none';
-	            document.body.style.overflow = 'auto';
-	            // Show notification for the first 5 seconds after overlay is disabled
-	            notification.classList.add('show');
-	            setTimeout(() => {
-	                notification.classList.remove('show');
-	            }, 5000);
-	        }, 2000); // Match the duration of the CSS animation
-	    };
+		const handleOverlayClick = () => {
+			overlayTop.classList.add('slide-up');
+			overlayBottom.classList.add('slide-down');
+			setTimeout(() => {
+				overlayCover.style.display = 'none';
+				document.body.style.overflow = 'auto';
+				// Show notification for the first 5 seconds after overlay is disabled
+				notification.classList.add('show');
+				setTimeout(() => {
+					notification.classList.remove('show');
+				}, 5000);
+			}, 2000); // Match the duration of the CSS animation
+		};
 
-	    const initPlayPauseButton = () => {
-	        playPauseBtn.addEventListener('click', handlePlayPause);
+		const initPlayPauseButton = () => {
+			playPauseBtn.addEventListener('click', handlePlayPause);
 
-	        // Play music after first user scroll
-	        window.addEventListener('click', function() {
-	            if (!isPlaying) {
-	                backgroundMusic.play();
-	                playPauseBtn.innerHTML = '<i class="icon-pause"></i>';
-	                playPauseBtn.classList.add('playing');
-	                isPlaying = true;
-	            }
-	        }, { once: true });
-	    };
+			// Play music after first user scroll
+			window.addEventListener('click', function () {
+				if (!isPlaying) {
+					backgroundMusic.play();
+					playPauseBtn.innerHTML = '<i class="icon-pause"></i>';
+					playPauseBtn.classList.add('playing');
+					isPlaying = true;
+				}
+			}, { once: true });
+		};
 
-	    const initOverlay = () => {
-	        // Block scrolling while overlay is active
-	        document.body.style.overflow = 'hidden';
+		const initOverlay = () => {
+			// Block scrolling while overlay is active
+			document.body.style.overflow = 'hidden';
 
-	        // Remove overlay and unblock scrolling after user clicks anywhere on the overlay
-	        overlayCover.addEventListener('click', handleOverlayClick);
-	    };
+			// Remove overlay and unblock scrolling after user clicks anywhere on the overlay
+			overlayCover.addEventListener('click', handleOverlayClick);
+		};
 
-	    initPlayPauseButton();
-	    initOverlay();
+		initPlayPauseButton();
+		initOverlay();
 	});
 
 }());
